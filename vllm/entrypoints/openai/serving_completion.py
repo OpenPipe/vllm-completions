11d76381071ef3660b39508a433b1157749ed8d3
Angky William <angkywilliam@Angkys-MacBook-Pro.local>
2024-11-21 11:31:03 -0800

    Copy changes from dynamic lora resolver

--- vllm/entrypoints/openai/serving_completion.py
+++ vllm/entrypoints/openai/serving_completion.py
@@ -26,8 +26,8 @@ from vllm.entrypoints.openai.serving_engine import (BaseModelPath,
                                                     LoRAModulePath,
                                                     OpenAIServing,
                                                     PromptAdapterPath)
-from vllm.lora.resolver import LoRAResolver
 from vllm.logger import init_logger
+from vllm.lora.resolver import LoRAResolver
 from vllm.outputs import RequestOutput
 from vllm.sequence import Logprob
 from vllm.tracing import (contains_trace_headers, extract_trace_headers,
